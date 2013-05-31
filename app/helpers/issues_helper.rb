5b7cf126990b4a795607e873044795217b073d4b
Ryan Lee <ryanlee@zepheira.com>
2013-05-30 18:40:32 -0700

    do not send edit updates by default

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -233,7 +233,7 @@ module IssuesHelper
                 content_tag(:label,
                             l(:label_notify_member_plural), :for => 'send_notification') +
                 hidden_field_tag('send_notification', '0', :id => nil) +
-                check_box_tag('send_notification', '1', true))
+                check_box_tag('send_notification', '1', false))
 
 
   end
